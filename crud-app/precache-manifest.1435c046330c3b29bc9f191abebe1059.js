bfa992b855161d80f241acbce5ba36244046f92d
Macbook <macbook@MacBook-Pro-de-Macbook.local>
2019-04-15 15:02:43 -0300

    separating folders and uploading crudApp

---
+++ crud-app/precache-manifest.1435c046330c3b29bc9f191abebe1059.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae9d2ce47a8eb93eef79",
+    "url": "/static/css/main.3d9b7c4c.chunk.css"
+  },
+  {
+    "revision": "ae9d2ce47a8eb93eef79",
+    "url": "/static/js/main.35a57c2c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "609637d649fbdf27206c",
+    "url": "/static/js/2.74462cb6.chunk.js"
+  },
+  {
+    "revision": "859a25c368d10c1e9bebdef0c617995b",
+    "url": "/index.html"
+  }
+];
