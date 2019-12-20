75e60adc0527a9eb9494aae60db50889bdd4040e
Lucas Palmieri <lucas.palmieri@superare.co>
2019-12-20 17:04:06 -0300

    Updates

---
+++ precache-manifest.bf4008810cf255021f9763b9449466c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4693dbb141d5509443124690afc093eb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "50bbd70a59b984c15d2e",
+    "url": "./static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "49bfcac88cf3e123832d",
+    "url": "./static/css/main.e994fef4.chunk.css"
+  },
+  {
+    "revision": "50bbd70a59b984c15d2e",
+    "url": "./static/js/2.2c2eaf0d.chunk.js"
+  },
+  {
+    "revision": "49bfcac88cf3e123832d",
+    "url": "./static/js/main.b10410e4.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "0f5429230c282ad289390bb38db4b8fd",
+    "url": "./static/media/profile.0f542923.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.807043c639bf1cd539f5d4e5783a971f.js"
+  "./precache-manifest.bf4008810cf255021f9763b9449466c5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
